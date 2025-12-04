ff4067e3fc123e326961b9ea7b60ce25b437ce26
IJing-WishSnow <wish.snowelf@gmail.com>
2025-12-05 00:42:35 +0800

    feat: add annotation

--- pledge-backend/schedule/services/poolService.go
+++ pledge-backend/schedule/services/poolService.go
@@ -15,139 +15,170 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+// poolService 资金池服务结构体，负责链上数据同步
 type poolService struct{}
 
+// NewPool 创建资金池服务实例
 func NewPool() *poolService {
 	return &poolService{}
 }
 
+// UpdateAllPoolInfo 更新所有资金池信息（测试网和主网）
+// 目前仅启用测试网同步，主网代码已被注释
 func (s *poolService) UpdateAllPoolInfo() {
-
+	// 更新测试网资金池信息
 	s.UpdatePoolInfo(config.Config.TestNet.PledgePoolToken, config.Config.TestNet.NetUrl, config.Config.TestNet.ChainId)
 
+	// 主网同步暂时注释，可按需启用
 	// s.UpdatePoolInfo(config.Config.MainNet.PledgePoolToken, config.Config.MainNet.NetUrl, config.Config.MainNet.ChainId)
-
 }
 
+// UpdatePoolInfo 更新指定链的资金池信息
+// contractAddress: 质押池合约地址
+// network: 区块链网络RPC URL
+// chainId: 链标识符
 func (s *poolService) UpdatePoolInfo(contractAddress, network, chainId string) {
 
-	log.Logger.Sugar().Info("UpdatePoolInfo ", contractAddress+" "+network)
+	log.Logger.Sugar().Info("开始更新资金池信息 ", contractAddress+" "+network)
+
+	// 1. 连接区块链网络
 	ethereumConn, err := ethclient.Dial(network)
 	if nil != err {
 		log.Logger.Error(err.Error())
 		return
 	}
+
+	// 2. 创建合约实例
 	pledgePoolToken, err := bindings.NewPledgePoolToken(common.HexToAddress(contractAddress), ethereumConn)
 	if nil != err {
 		log.Logger.Error(err.Error())
 		return
 	}
 
-	// borrowFee
+	// 3. 获取全局费率参数（借款费和贷款费）
 	borrowFee, err := pledgePoolToken.PledgePoolTokenCaller.BorrowFee(nil)
-
-	// lendFee
 	lendFee, err := pledgePoolToken.PledgePoolTokenCaller.LendFee(nil)
 
-	//poolLength
+	// 4. 获取资金池总数
 	pLength, err := pledgePoolToken.PledgePoolTokenCaller.PoolLength(nil)
 	if nil != err {
 		log.Logger.Error(err.Error())
 		return
 	}
 
+	// 5. 遍历所有资金池，同步数据
 	for i := 0; i <= int(pLength.Int64())-1; i++ {
 
-		log.Logger.Sugar().Info("UpdatePoolInfo ", i)
+		log.Logger.Sugar().Info("正在更新资金池 ", i)
 		poolId := utils.IntToString(i + 1)
+
+		// 5.1 获取资金池基础信息
 		baseInfo, err := pledgePoolToken.PledgePoolTokenCaller.PoolBaseInfo(nil, big.NewInt(int64(i)))
 		if err != nil {
-			log.Logger.Sugar().Info("UpdatePoolInfo PoolBaseInfo err", poolId, err)
+			log.Logger.Sugar().Info("获取资金池基础信息失败 ", poolId, err)
 			continue
 		}
 
+		// 5.2 获取借款和贷款代币的详细信息
 		_, borrowToken := models.NewTokenInfo().GetTokenInfo(baseInfo.BorrowToken.String(), chainId)
 		_, lendToken := models.NewTokenInfo().GetTokenInfo(baseInfo.LendToken.String(), chainId)
 
+		// 5.3 构建代币信息的JSON字符串
 		lendTokenJson, _ := json.Marshal(models.LendToken{
-			LendFee:    lendFee.String(),
-			TokenLogo:  lendToken.Logo,
-			TokenName:  lendToken.Symbol,
-			TokenPrice: lendToken.Price,
+			LendFee:    lendFee.String(), // 贷款手续费
+			TokenLogo:  lendToken.Logo,   // 代币Logo
+			TokenName:  lendToken.Symbol, // 代币名称
+			TokenPrice: lendToken.Price,  // 代币价格
 		})
 		borrowTokenJson, _ := json.Marshal(models.BorrowToken{
-			BorrowFee:  borrowFee.String(),
-			TokenLogo:  borrowToken.Logo,
-			TokenName:  borrowToken.Symbol,
-			TokenPrice: borrowToken.Price,
+			BorrowFee:  borrowFee.String(), // 借款手续费
+			TokenLogo:  borrowToken.Logo,   // 代币Logo
+			TokenName:  borrowToken.Symbol, // 代币名称
+			TokenPrice: borrowToken.Price,  // 代币价格
 		})
 
+		// 5.4 构建资金池基础信息模型
 		poolBase := models.PoolBase{
-			SettleTime:             baseInfo.SettleTime.String(),
-			PoolId:                 utils.StringToInt(poolId),
-			ChainId:                chainId,
-			EndTime:                baseInfo.EndTime.String(),
-			InterestRate:           baseInfo.InterestRate.String(),
-			MaxSupply:              baseInfo.MaxSupply.String(),
-			LendSupply:             baseInfo.LendSupply.String(),
-			BorrowSupply:           baseInfo.BorrowSupply.String(),
-			MartgageRate:           baseInfo.MartgageRate.String(),
-			LendToken:              baseInfo.LendToken.String(),
-			LendTokenSymbol:        lendToken.Symbol,
-			LendTokenInfo:          string(lendTokenJson),
-			BorrowToken:            baseInfo.BorrowToken.String(),
-			BorrowTokenSymbol:      borrowToken.Symbol,
-			BorrowTokenInfo:        string(borrowTokenJson),
-			State:                  utils.IntToString(int(baseInfo.State)),
-			SpCoin:                 baseInfo.SpCoin.String(),
-			JpCoin:                 baseInfo.JpCoin.String(),
-			AutoLiquidateThreshold: baseInfo.AutoLiquidateThreshold.String(),
+			SettleTime:             baseInfo.SettleTime.String(),             // 结算时间
+			PoolId:                 utils.StringToInt(poolId),                // 资金池ID
+			ChainId:                chainId,                                  // 链ID
+			EndTime:                baseInfo.EndTime.String(),                // 结束时间
+			InterestRate:           baseInfo.InterestRate.String(),           // 利率
+			MaxSupply:              baseInfo.MaxSupply.String(),              // 最大供应量
+			LendSupply:             baseInfo.LendSupply.String(),             // 贷款供应量
+			BorrowSupply:           baseInfo.BorrowSupply.String(),           // 借款供应量
+			MartgageRate:           baseInfo.MartgageRate.String(),           // 抵押率
+			LendToken:              baseInfo.LendToken.String(),              // 贷款代币地址
+			LendTokenSymbol:        lendToken.Symbol,                         // 贷款代币符号
+			LendTokenInfo:          string(lendTokenJson),                    // 贷款代币详细信息(JSON)
+			BorrowToken:            baseInfo.BorrowToken.String(),            // 借款代币地址
+			BorrowTokenSymbol:      borrowToken.Symbol,                       // 借款代币符号
+			BorrowTokenInfo:        string(borrowTokenJson),                  // 借款代币详细信息(JSON)
+			State:                  utils.IntToString(int(baseInfo.State)),   // 资金池状态
+			SpCoin:                 baseInfo.SpCoin.String(),                 // SP代币数量
+			JpCoin:                 baseInfo.JpCoin.String(),                 // JP代币数量
+			AutoLiquidateThreshold: baseInfo.AutoLiquidateThreshold.String(), // 自动清算阈值
 		}
 
+		// 5.5 通过MD5比较判断数据是否有变化
 		hasInfoData, byteBaseInfoStr, baseInfoMd5Str := s.GetPoolMd5(&poolBase, "base_info:pool_"+chainId+"_"+poolId)
-		if !hasInfoData || (baseInfoMd5Str != byteBaseInfoStr) { // have new data
-			//tokenInfo
+		if !hasInfoData || (baseInfoMd5Str != byteBaseInfoStr) { // 数据不存在或有变化
+			// 保存新的资金池基础信息到数据库
 			err = models.NewPoolBase().SavePoolBase(chainId, poolId, &poolBase)
 			if err != nil {
-				log.Logger.Sugar().Error("SavePoolBase err ", chainId, poolId)
+				log.Logger.Sugar().Error("保存资金池基础信息失败 ", chainId, poolId)
 			}
-			_ = db.RedisSet("base_info:pool_"+chainId+"_"+poolId, baseInfoMd5Str, 60*30) //The expiration time is set to prevent hsah collision
+			// 更新Redis缓存，设置30分钟过期时间（防止哈希冲突）
+			_ = db.RedisSet("base_info:pool_"+chainId+"_"+poolId, baseInfoMd5Str, 60*30)
 		}
 
+		// 5.6 获取资金池数据信息
 		dataInfo, err := pledgePoolToken.PledgePoolTokenCaller.PoolDataInfo(nil, big.NewInt(int64(i)))
 		if err != nil {
-			log.Logger.Sugar().Info("UpdatePoolInfo PoolBaseInfo err", poolId, err)
+			log.Logger.Sugar().Info("获取资金池数据信息失败 ", poolId, err)
 			continue
 		}
 
+		// 5.7 通过MD5比较判断资金池数据是否有变化
 		hasPoolData, byteDataInfoStr, dataInfoMd5Str := s.GetPoolMd5(&poolBase, "data_info:pool_"+chainId+"_"+poolId)
-		if !hasPoolData || (dataInfoMd5Str != byteDataInfoStr) { // have new data
+		if !hasPoolData || (dataInfoMd5Str != byteDataInfoStr) { // 数据不存在或有变化
 			poolData := models.PoolData{
-				PoolId:                 poolId,
-				ChainId:                chainId,
-				FinishAmountBorrow:     dataInfo.FinishAmountBorrow.String(),
-				FinishAmountLend:       dataInfo.FinishAmountLend.String(),
-				LiquidationAmounBorrow: dataInfo.LiquidationAmounBorrow.String(),
-				LiquidationAmounLend:   dataInfo.LiquidationAmounLend.String(),
-				SettleAmountBorrow:     dataInfo.SettleAmountBorrow.String(),
-				SettleAmountLend:       dataInfo.SettleAmountLend.String(),
+				PoolId:                 poolId,                                   // 资金池ID
+				ChainId:                chainId,                                  // 链ID
+				FinishAmountBorrow:     dataInfo.FinishAmountBorrow.String(),     // 已完成借款金额
+				FinishAmountLend:       dataInfo.FinishAmountLend.String(),       // 已完成贷款金额
+				LiquidationAmounBorrow: dataInfo.LiquidationAmounBorrow.String(), // 清算借款金额
+				LiquidationAmounLend:   dataInfo.LiquidationAmounLend.String(),   // 清算贷款金额
+				SettleAmountBorrow:     dataInfo.SettleAmountBorrow.String(),     // 结算借款金额
+				SettleAmountLend:       dataInfo.SettleAmountLend.String(),       // 结算贷款金额
 			}
+			// 保存资金池数据信息到数据库
 			err = models.NewPoolData().SavePoolData(chainId, poolId, &poolData)
 			if err != nil {
-				log.Logger.Sugar().Error("SavePoolData err ", chainId, poolId)
+				log.Logger.Sugar().Error("保存资金池数据信息失败 ", chainId, poolId)
 			}
-			_ = db.RedisSet("data_info:pool_"+chainId+"_"+poolId, dataInfoMd5Str, 60*30) //The expiration time is set to prevent hsah collision
+			// 更新Redis缓存，设置30分钟过期时间
+			_ = db.RedisSet("data_info:pool_"+chainId+"_"+poolId, dataInfoMd5Str, 60*30)
 		}
 	}
 }
 
+// GetPoolMd5 获取资金池信息的MD5哈希值，用于判断数据是否变更
+// baseInfo: 资金池基础信息结构体指针
+// key: Redis缓存键名
+// 返回值: (数据是否存在, 缓存的MD5值, 当前数据的MD5值)
 func (s *poolService) GetPoolMd5(baseInfo *models.PoolBase, key string) (bool, string, string) {
+	// 将结构体序列化为JSON并计算MD5
 	baseInfoBytes, _ := json.Marshal(baseInfo)
 	baseInfoMd5Str := utils.Md5(string(baseInfoBytes))
+
+	// 从Redis获取缓存的MD5值
 	resInfoBytes, _ := db.RedisGet(key)
 	if len(resInfoBytes) > 0 {
+		// 返回true（数据存在）、缓存的MD5、当前的MD5
 		return true, strings.Trim(string(resInfoBytes), `"`), baseInfoMd5Str
 	} else {
+		// 返回false（数据不存在）、空字符串、当前的MD5
 		return false, strings.Trim(string(resInfoBytes), `"`), baseInfoMd5Str
 	}
 }
--- pledge-backend/schedule/services/tokenSymbolService.go
+++ pledge-backend/schedule/services/tokenSymbolService.go
@@ -19,13 +19,15 @@ import (
 	"gorm.io/gorm"
 )
 
+// TokenSymbol 代币符号服务结构体
 type TokenSymbol struct{}
 
+// NewTokenSymbol 创建代币符号服务实例
 func NewTokenSymbol() *TokenSymbol {
 	return &TokenSymbol{}
 }
 
-// UpdateContractSymbol get contract symbol
+// UpdateContractSymbol get contract symbol / 更新代币合约符号
 func (s *TokenSymbol) UpdateContractSymbol() {
 	var tokens []models.TokenInfo
 	db.Mysql.Table("token_info").Find(&tokens)
@@ -36,9 +38,11 @@ func (s *TokenSymbol) UpdateContractSymbol() {
 		}
 		err := errors.New("")
 		symbol := ""
+		// 根据链ID决定从测试网或主网获取代币符号
 		if t.ChainId == config.Config.TestNet.ChainId {
 			err, symbol = s.GetContractSymbolOnTestNet(t.Token, config.Config.TestNet.NetUrl)
 		} else if t.ChainId == config.Config.MainNet.ChainId {
+			// 主网需要检查ABI文件是否存在
 			if t.AbiFileExist == 0 {
 				err = s.GetRemoteAbiFileByToken(t.Token, t.ChainId)
 				if err != nil {
@@ -46,7 +50,7 @@ func (s *TokenSymbol) UpdateContractSymbol() {
 					continue
 				}
 			}
-			err, symbol = s.GetContractSymbolOnMainNet(t.Token, config.Config.MainNet.NetUrl)
+			err, symbol = s.GetContractSymbolOnTestNet(t.Token, config.Config.MainNet.NetUrl)
 		} else {
 			log.Logger.Sugar().Error("UpdateContractSymbol chain_id err ", t.Symbol, t.ChainId)
 			continue
@@ -56,12 +60,14 @@ func (s *TokenSymbol) UpdateContractSymbol() {
 			continue
 		}
 
+		// 检查是否有新的符号数据需要保存
 		hasNewData, err := s.CheckSymbolData(t.Token, t.ChainId, symbol)
 		if err != nil {
 			log.Logger.Sugar().Error("UpdateContractSymbol CheckSymbolData err ", err)
 			continue
 		}
 
+		// 如果有新数据则保存到数据库
 		if hasNewData {
 			err = s.SaveSymbolData(t.Token, t.ChainId, symbol)
 			if err != nil {
@@ -72,19 +78,21 @@ func (s *TokenSymbol) UpdateContractSymbol() {
 	}
 }
 
-// GetRemoteAbiFileByToken get and save remote abi file on main net
+// GetRemoteAbiFileByToken get and save remote abi file on main net / 从远程获取并保存主网的ABI文件
 func (s *TokenSymbol) GetRemoteAbiFileByToken(token, chainId string) error {
 
 	// url := "https://api.bscscan.com/api?module=contract&action=getabi&apikey=HJ3WS4N88QJ6S7PQ8D89BD49IZIFP1JFER&address=" + token
 
 	url := "https://api-sepolia.etherscan.io/api?module=contract&action=getabi&address=" + token
 
+	// 发送HTTP请求获取ABI JSON
 	res, err := utils.HttpGet(url, map[string]string{})
 	if err != nil {
 		log.Logger.Error(err.Error())
 		return err
 	}
 
+	// 格式化ABI JSON字符串
 	resStr := s.FormatAbiJsonStr(string(res))
 
 	abiJson := models.AbiJson{}
@@ -94,18 +102,20 @@ func (s *TokenSymbol) GetRemoteAbiFileByToken(token, chainId string) error {
 		return err
 	}
 
+	// 检查API返回状态
 	if abiJson.Status != "1" {
 		log.Logger.Sugar().Error("get remote abi file failed: status 0 ", resStr)
 		return errors.New("get remote abi file failed: status 0 ")
 	}
 
-	// marshal and format
+	// 序列化并格式化ABI结果
 	abiJsonBytes, err := json.MarshalIndent(abiJson.Result, "", "\t")
 	if err != nil {
 		log.Logger.Error(err.Error())
 		return err
 	}
 
+	// 构建ABI文件路径并写入文件
 	newAbiFile := abifile.GetCurrentAbPathByCaller() + "/" + token + ".abi"
 
 	err = os.WriteFile(newAbiFile, abiJsonBytes, 0777)
@@ -114,6 +124,7 @@ func (s *TokenSymbol) GetRemoteAbiFileByToken(token, chainId string) error {
 		return err
 	}
 
+	// 更新数据库标记，表示ABI文件已存在
 	err = db.Mysql.Table("token_info").Where("token=? and chain_id=?", token, chainId).Updates(map[string]interface{}{
 		"abi_file_exist": 1,
 	}).Debug().Error
@@ -123,7 +134,7 @@ func (s *TokenSymbol) GetRemoteAbiFileByToken(token, chainId string) error {
 	return nil
 }
 
-// FormatAbiJsonStr format the abi string
+// FormatAbiJsonStr format the abi string / 格式化ABI JSON字符串
 func (s *TokenSymbol) FormatAbiJsonStr(result string) string {
 	resStr := strings.Replace(result, `\`, ``, -1)
 	resStr = strings.Replace(result, `\"`, `"`, -1)
@@ -132,29 +143,34 @@ func (s *TokenSymbol) FormatAbiJsonStr(result string) string {
 	return resStr
 }
 
-// GetContractSymbolOnMainNet get contract symbol on main net
+// GetContractSymbolOnMainNet get contract symbol on main net / 在主网上获取代币合约符号
 func (s *TokenSymbol) GetContractSymbolOnMainNet(token, network string) (error, string) {
+	// 连接以太坊网络
 	ethereumConn, err := ethclient.Dial(network)
 	if nil != err {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
+	// 通过代币地址获取ABI
 	abiStr, err := abifile.GetAbiByToken(token)
 	if err != nil {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
+	// 解析ABI字符串
 	parsed, err := abi.JSON(strings.NewReader(abiStr))
 	if err != nil {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
+	// 创建绑定合约实例
 	contract, err := bind.NewBoundContract(common.HexToAddress(token), parsed, ethereumConn, ethereumConn, ethereumConn), nil
 	if err != nil {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
 
+	// 调用合约的symbol方法
 	res := make([]interface{}, 0)
 	err = contract.Call(nil, &res, "symbol")
 	if err != nil {
@@ -165,29 +181,34 @@ func (s *TokenSymbol) GetContractSymbolOnMainNet(token, network string) (error, 
 	return nil, res[0].(string)
 }
 
-// GetContractSymbolOnTestNet get contract symbol on test net
+// GetContractSymbolOnTestNet get contract symbol on test net / 在测试网上获取代币合约符号
 func (s *TokenSymbol) GetContractSymbolOnTestNet(token, network string) (error, string) {
+	// 连接以太坊网络
 	ethereumConn, err := ethclient.Dial(network)
 	if nil != err {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
+	// 使用标准的ERC20 ABI文件
 	abiStr, err := abifile.GetAbiByToken("erc20")
 	if err != nil {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
+	// 解析ABI字符串
 	parsed, err := abi.JSON(strings.NewReader(abiStr))
 	if err != nil {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
+	// 创建绑定合约实例
 	contract, err := bind.NewBoundContract(common.HexToAddress(token), parsed, ethereumConn, ethereumConn, ethereumConn), nil
 	if err != nil {
 		log.Logger.Sugar().Error("GetContractSymbolOnMainNet err ", token, err)
 		return err, ""
 	}
 
+	// 调用合约的symbol方法
 	res := make([]interface{}, 0)
 	err = contract.Call(nil, &res, "symbol")
 	if err != nil {
@@ -198,15 +219,17 @@ func (s *TokenSymbol) GetContractSymbolOnTestNet(token, network string) (error, 
 	return nil, res[0].(string)
 }
 
-// CheckSymbolData Saving symbol data to redis if it has new symbol
+// CheckSymbolData Saving symbol data to redis if it has new symbol / 检查并保存符号数据到Redis
 func (s *TokenSymbol) CheckSymbolData(token, chainId, symbol string) (bool, error) {
 	redisKey := "token_info:" + chainId + ":" + token
 	redisTokenInfoBytes, err := db.RedisGet(redisKey)
 	if len(redisTokenInfoBytes) <= 0 {
+		// Redis中没有数据，先检查数据库
 		err = s.CheckTokenInfo(token, chainId)
 		if err != nil {
 			log.Logger.Error(err.Error())
 		}
+		// 将新数据存入Redis
 		err = db.RedisSet(redisKey, models.RedisTokenInfo{
 			Token:   token,
 			ChainId: chainId,
@@ -217,6 +240,7 @@ func (s *TokenSymbol) CheckSymbolData(token, chainId, symbol string) (bool, erro
 			return false, err
 		}
 	} else {
+		// Redis中已有数据，进行反序列化
 		redisTokenInfo := models.RedisTokenInfo{}
 		err = json.Unmarshal(redisTokenInfoBytes, &redisTokenInfo)
 		if err != nil {
@@ -224,10 +248,12 @@ func (s *TokenSymbol) CheckSymbolData(token, chainId, symbol string) (bool, erro
 			return false, err
 		}
 
+		// 检查符号是否相同
 		if redisTokenInfo.Symbol == symbol {
 			return false, nil
 		}
 
+		// 符号不同则更新Redis缓存
 		redisTokenInfo.Symbol = symbol
 		err = db.RedisSet(redisKey, redisTokenInfo, 0)
 		if err != nil {
@@ -238,12 +264,13 @@ func (s *TokenSymbol) CheckSymbolData(token, chainId, symbol string) (bool, erro
 	return true, nil
 }
 
-// CheckTokenInfo  Insert token information if it was not in mysql
+// CheckTokenInfo  Insert token information if it was not in mysql / 检查并插入代币信息到数据库
 func (s *TokenSymbol) CheckTokenInfo(token, chainId string) error {
 	tokenInfo := models.TokenInfo{}
 	err := db.Mysql.Table("token_info").Where("token=? and chain_id=?", token, chainId).First(&tokenInfo).Debug().Error
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
+			// 记录不存在，创建新记录
 			tokenInfo = models.TokenInfo{}
 			nowDateTime := utils.GetCurDateTimeFormat()
 			tokenInfo.Token = token
@@ -261,10 +288,11 @@ func (s *TokenSymbol) CheckTokenInfo(token, chainId string) error {
 	return nil
 }
 
-// SaveSymbolData Saving symbol data to mysql if it has new symbol
+// SaveSymbolData Saving symbol data to mysql if it has new symbol / 保存符号数据到MySQL数据库
 func (s *TokenSymbol) SaveSymbolData(token, chainId, symbol string) error {
 	nowDateTime := utils.GetCurDateTimeFormat()
 
+	// 更新数据库中的代币符号
 	err := db.Mysql.Table("token_info").Where("token=? and chain_id=? ", token, chainId).Updates(map[string]interface{}{
 		"symbol":     symbol,
 		"updated_at": nowDateTime,
